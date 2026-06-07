368d160a1c1c859f9eefd9d3657adf9b3d43fcc9
David Cvetkovski <david.cvetkovski@gmail.com>
2026-06-07 04:46:59 +0200

    Allow custom domain in CORS

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://lyricstats.dev",
+        "https://www.lyricstats.dev",
     ],
     allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=False,
